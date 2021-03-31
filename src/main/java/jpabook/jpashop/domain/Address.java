276619bcfa731c5fc6d28ddda3489191b9819ee6
312a <cnwlsguq1@naver.com>
2021-03-31 12:58:24 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.domain;
+
+import lombok.*;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@AllArgsConstructor @NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+}
